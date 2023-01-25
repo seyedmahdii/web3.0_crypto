82579762e699ca8b998a069c09842aa6e6548846
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2023-01-25 16:03:28 +0330

    Init Project

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  // 17:00
+  return (
+    <div className="App">
+        <h1 className="text-3xl font-bold underline strike color-red">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
